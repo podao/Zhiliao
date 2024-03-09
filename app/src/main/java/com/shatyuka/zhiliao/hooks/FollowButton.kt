7f88644ce1e1af0b10685805a8f3de4c832cd1c3
CLOUDERHEM <cloudac@qq.com>
2024-03-09 22:25:16 +0800

    refactor(FollowButton): 重构为kotlin

--- app/src/main/java/com/shatyuka/zhiliao/hooks/FollowButton.kt
+++ app/src/main/java/com/shatyuka/zhiliao/hooks/FollowButton.kt
@@ -1,77 +1,75 @@
-package com.shatyuka.zhiliao.hooks;
+package com.shatyuka.zhiliao.hooks
 
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.FrameLayout;
-
-import com.shatyuka.zhiliao.Helper;
-
-import java.lang.reflect.Field;
-import java.util.Arrays;
-
-import de.robv.android.xposed.XC_MethodHook;
-import de.robv.android.xposed.XposedBridge;
+import android.view.View
+import android.view.ViewGroup
+import android.widget.FrameLayout
+import com.shatyuka.zhiliao.Helper
+import de.robv.android.xposed.XC_MethodHook
+import de.robv.android.xposed.XposedBridge
+import java.lang.reflect.Field
 
 /**
  * 卡片视图去除关注按钮
  * todo: 整合web去除关注
  */
-public class FollowButton implements IHook {
-    static Class<?> followWithAvatarView;
-    static Class<?> bottomReactionView;
-    static Class<?> followPeopleButton;
-    static Class<?> zHAuthorInfoView;
+class FollowButton : IHook {
 
-    static Field followWithAvatarViewField;
-    static Field followPeopleButtonField;
+    private lateinit var followWithAvatarView: Class<*>
+    private lateinit var bottomReactionView: Class<*>
+    private lateinit var followPeopleButton: Class<*>
+    private lateinit var zHAuthorInfoView: Class<*>
+    private lateinit var followWithAvatarViewField: Field
+    private lateinit var followPeopleButtonField: Field
 
-    @Override
-    public String getName() {
-        return "去关注按钮";
+    override fun getName(): String {
+        return "去关注按钮"
     }
 
-    @Override
-    public void init(ClassLoader classLoader) throws Throwable {
-        followWithAvatarView = classLoader.loadClass("com.zhihu.android.unify_interactive.view.follow.FollowWithAvatarView");
-        bottomReactionView = classLoader.loadClass("com.zhihu.android.mixshortcontainer.function.mixup.view.BottomReactionView");
+    @Throws(Throwable::class)
+    override fun init(classLoader: ClassLoader) {
+        followWithAvatarView =
+            classLoader.loadClass("com.zhihu.android.unify_interactive.view.follow.FollowWithAvatarView")
+        bottomReactionView =
+            classLoader.loadClass("com.zhihu.android.mixshortcontainer.function.mixup.view.BottomReactionView")
+        followPeopleButton =
+            classLoader.loadClass("com.zhihu.android.unify_interactive.view.follow.FollowPeopleButton")
+        zHAuthorInfoView =
+            classLoader.loadClass("com.zhihu.android.mixshortcontainer.function.mixup.author.ZHAuthorInfoView")
 
-        followPeopleButton = classLoader.loadClass("com.zhihu.android.unify_interactive.view.follow.FollowPeopleButton");
-        zHAuthorInfoView = classLoader.loadClass("com.zhihu.android.mixshortcontainer.function.mixup.author.ZHAuthorInfoView");
-
-        followWithAvatarViewField = findFieldByType(bottomReactionView, followWithAvatarView);
-        followPeopleButtonField = findFieldByType(zHAuthorInfoView, followPeopleButton);
+        followWithAvatarViewField = Helper.findFieldByType(bottomReactionView, followWithAvatarView)
+        followPeopleButtonField = Helper.findFieldByType(zHAuthorInfoView, followPeopleButton)
     }
 
-    @Override
-    public void hook() throws Throwable {
-        XposedBridge.hookAllMethods(bottomReactionView, "setData", new XC_MethodHook() {
-            @Override
-            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                if (Helper.prefs.getBoolean("switch_mainswitch", false) && Helper.prefs.getBoolean("switch_subscribe", false)) {
+    @Throws(Throwable::class)
+    override fun hook() {
+        XposedBridge.hookAllMethods(bottomReactionView, "setData", object : XC_MethodHook() {
+            @Throws(Throwable::class)
+            override fun afterHookedMethod(param: MethodHookParam) {
+                if (Helper.prefs.getBoolean("switch_mainswitch", false)
+                    && Helper.prefs.getBoolean("switch_subscribe", false)
+                ) {
                     // 去除底部关注(带头像)
-                    FrameLayout followWithAvatarViewInstance = (FrameLayout) followWithAvatarViewField.get(param.thisObject);
-                    followWithAvatarViewInstance.setVisibility(View.GONE);
+                    val followWithAvatarViewInstance =
+                        followWithAvatarViewField.get(param.thisObject) as FrameLayout
+                    followWithAvatarViewInstance.visibility = View.GONE
                 }
             }
-        });
+        })
 
-        XposedBridge.hookAllMethods(zHAuthorInfoView, "setData", new XC_MethodHook() {
-            @Override
-            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                if (Helper.prefs.getBoolean("switch_mainswitch", false) && Helper.prefs.getBoolean("switch_subscribe", false)) {
+        XposedBridge.hookAllMethods(zHAuthorInfoView, "setData", object : XC_MethodHook() {
+            @Throws(Throwable::class)
+            override fun afterHookedMethod(param: MethodHookParam) {
+                if (Helper.prefs.getBoolean("switch_mainswitch", false)
+                    && Helper.prefs.getBoolean("switch_subscribe", false)
+                ) {
                     // 去除顶部关注
-                    ViewGroup followPeopleButtonFieldInstance = (ViewGroup) followPeopleButtonField.get(param.thisObject);
-                    followPeopleButtonFieldInstance.setVisibility(View.GONE);
+                    val followPeopleButtonFieldInstance =
+                        followPeopleButtonField.get(param.thisObject) as ViewGroup
+                    followPeopleButtonFieldInstance.visibility = View.GONE
                 }
             }
-        });
+        })
     }
 
-    private Field findFieldByType(Class<?> clazz, Class<?> type) {
-        Field field = Arrays.stream(clazz.getDeclaredFields())
-                .filter(f -> f.getType() == type).findFirst().get();
 
-        field.setAccessible(true);
-        return field;
-    }
 }
