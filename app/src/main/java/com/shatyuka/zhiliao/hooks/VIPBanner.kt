f2a29392cfa8dd4cbc728bfa25576ea7986da04c
CLOUDERHEM <cloudac@qq.com>
2024-11-02 14:45:09 +0800

    fix: 隐藏会员卡片功能失效

--- app/src/main/java/com/shatyuka/zhiliao/hooks/VIPBanner.kt
+++ app/src/main/java/com/shatyuka/zhiliao/hooks/VIPBanner.kt
@@ -1,57 +1,41 @@
 package com.shatyuka.zhiliao.hooks
 
 import com.shatyuka.zhiliao.Helper
-import de.robv.android.xposed.XC_MethodReplacement.DO_NOTHING
 import de.robv.android.xposed.XC_MethodReplacement.returnConstant
 import de.robv.android.xposed.XposedBridge.hookAllMethods
-import de.robv.android.xposed.XposedBridge.hookMethod
-import java.lang.reflect.Method
 
 class VIPBanner : BaseHook() {
 
-    private var handleVipData: Method? = null
-    private var moreVipData: Class<*>? = null
+    private var moreVipDataList: MutableList<Class<*>> = ArrayList()
+
+    private var moreVipDataClassNameList = listOf<String>(
+        "com.zhihu.android.api.MoreVipData",
+        "com.zhihu.android.profile.data.model.MoreVipData",
+        "com.zhihu.android.app.ui.fragment.more.mine.model.MoreKVipData"
+    )
 
     override fun getName(): String {
         return "隐藏会员卡片"
     }
 
-    @Throws(Throwable::class)
     override fun init(classLoader: ClassLoader) {
-        try {
-            moreVipData = try {
-                classLoader.loadClass("com.zhihu.android.api.MoreVipData")
-            } catch (ignored: ClassNotFoundException) {
-                classLoader.loadClass("com.zhihu.android.profile.data.model.MoreVipData")
+        moreVipDataClassNameList.forEach {
+            try {
+                moreVipDataList.add(classLoader.loadClass(it))
+            } catch (e: ClassNotFoundException) {
+                logE(e.message)
             }
-        } catch (e: Exception) {
-            logE(e)
-        }
-
-        try {
-            val mineTabFragment =
-                classLoader.loadClass("com.zhihu.android.app.ui.fragment.more.mine.MineTabFragment")
-            handleVipData = Helper.getMethodByParameterTypes(mineTabFragment, moreVipData)
-        } catch (e: Exception) {
-            logE(e)
         }
     }
 
-    @Throws(Throwable::class)
     override fun hook() {
         if (!Helper.prefs.getBoolean("switch_mainswitch", false)
             || !Helper.prefs.getBoolean("switch_vipbanner", false)
         ) {
             return
         }
-
-        if (handleVipData != null) {
-            hookMethod(handleVipData, DO_NOTHING)
+        moreVipDataList.forEach {
+            hookAllMethods(it, "isLegal", returnConstant(false))
         }
-
-        if (moreVipData != null) {
-            hookAllMethods(moreVipData, "isLegal", returnConstant(false))
-        }
-
     }
 }
